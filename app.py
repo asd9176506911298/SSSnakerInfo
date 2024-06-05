2364986a976d59aaff7df350378d1df579a7367d
Yuki.kaco <a0936660023@gmail.com>
2024-06-06 04:40:18 +0800

    Add Login Image

--- app.py
+++ app.py
@@ -10,16 +10,32 @@ app = Flask(__name__)
 def index():
     # URL of the JSON file within a zip archive
     URL = "https://res.snakesvc.com/assets/res.json"
-    
+    assetsUrl = 'https://res.snakesvc.com/assets'
     # Fetch the zip file from the URL
     response = requests.get(URL)
     
+    login = []
+
     # Open the zip file from the response content
     with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file:
         # Read the JSON file within the zip archive
         with zip_file.open('res.json') as json_file:
             content = json_file.read()
             res = json.loads(content.decode("utf-8"))
+            print('downloaded')
+            for i in res['files']:
+                if i.startswith('login'):
+                    if i.endswith('.png'):
+                        md5 = res['files'][i]['md5']
+                        size = res['files'][i]['size']
+                        path_Dot = i.split('.')
+                        imageName = path_Dot[0] + '.' + md5 + '.' + path_Dot[1]
+                        filePath = assetsUrl + '/' + imageName
+                        login.append((filePath, size))
+                        print(filePath, size)
+    
+    login_sorted = sorted(login, key=lambda x: x[1], reverse=True)
+    print(login_sorted[0])
     
     # Extract version information
     version = res['version']
@@ -27,11 +43,13 @@ def index():
         f"branch: {version['branch']}<br>"
         f"short_id: {version['short_id']}<br>"
         f"datetime: {version['datetime']}<br>"
-        f"msg: {version['msg']}"
+        f"msg: {version['msg']}<br>"
     )
+
+    # Construct HTML for clickable hyperlink
+    html_output = f'{version_data}Login Image:<a href="{login_sorted[0][0]}">{login_sorted[0][0]}</a><br>'
     
-    # Return the formatted version data as an HTML response
-    return Response(version_data, mimetype='text/html')
+    return html_output
 
 if __name__ == '__main__':
     app.run(debug=True)
