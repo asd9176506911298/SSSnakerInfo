f1bb05e75cfc120c498da3945df8d89c09fa7c44
Yuki.kaco <a0936660023@gmail.com>
2024-07-06 16:32:19 +0800

    Add Query shortid

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
 import zipfile
 import json
@@ -53,6 +53,10 @@ def index():
         <form action="/getmainScenePicture" method="post">
             <button type="submit" style="width: 300px; height: 50px; font-size: 20px; padding: 10px 20px;">Get Main Scene Pictures</button>
         </form>
+        <form action="/query" method="post">
+            <input type="text" name="short_id" placeholder="Enter short_id" required>
+            <button type="submit" style="width: 100px; height: 30px; font-size: 16px; padding: 5px 10px;">Query</button>
+        </form>
     '''
     return html_output
 
@@ -91,6 +95,60 @@ def getmainScenePicture():
     html_output += "</div>"
     
     return html_output
+
+@app.route('/query', methods=['POST'])
+def query_short_id():
+    assetsUrl = 'https://res.snakesvc.com/assets'
+    short_id = request.form['short_id']
+    fileName = f'res.{short_id}.json'
+    URL = f'https://res.snakesvc.com/res-version/{fileName}'
+    response = requests.get(URL)
+
+    login = []
+    
+    # Open the zip file from the response content
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_file: 
+        # Read the JSON file within the zip archive
+        with zip_file.open(fileName) as json_file:
+            content = json_file.read()
+            res = json.loads(content.decode("utf-8"))
+            for i in res['files']:
+                if i.startswith('login'):
+                    if i.endswith('.png'):
+                        md5 = res['files'][i]['md5']
+                        size = res['files'][i]['size']
+                        path_Dot = i.split('.')
+                        imageName = path_Dot[0] + '.' + md5 + '.' + path_Dot[1]
+                        filePath = assetsUrl + '/' + imageName
+                        login.append((filePath, size))
+                        print(filePath, size)
+    
+    login_sorted = sorted(login, key=lambda x: x[1], reverse=True)
+    print(login_sorted[0])
+    
+    # Extract version information
+    version = res['version']
+    version_data = (
+        f"branch: {version['branch']}<br>"
+        f"short_id: {version['short_id']}<br>"
+        f"datetime: {version['datetime']}<br>"
+        f"msg: {version['msg']}<br>"
+    )
+
+    # Construct HTML to display version information and login image
+    html_output = f'{version_data}Login Image:<br>'
+    html_output += f'<img src="{login_sorted[0][0]}" alt="Login Image" style="max-width: 500px;">'
+    html_output += '''
+        <form action="/getmainScenePicture" method="post">
+            <button type="submit" style="width: 300px; height: 50px; font-size: 20px; padding: 10px 20px;">Get Main Scene Pictures</button>
+        </form>
+        <form action="/query" method="post">
+            <input type="text" name="short_id" placeholder="Enter short_id" required>
+            <button type="submit" style="width: 100px; height: 30px; font-size: 16px; padding: 5px 10px;">Query</button>
+        </form>
+    '''
+    return html_output
+
 
 if __name__ == '__main__':
     app.run(debug=True)
